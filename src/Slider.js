789e23feb36f8898043f3016590505c3617945bc
HinaM <ruby890918@gmail.com>
2024-09-07 04:15:29 +0800

    2024/09/07

--- src/Slider.js
+++ src/Slider.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleLeft, faCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay,Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 import gallery1 from './img/蘇伍柒_靜理央.jpg';
 import gallery2 from './img/哺會通靈_MSK.png';
 import gallery3 from './img/巴拉魚_靜理央.png';
@@ -18,22 +18,55 @@ import gallery7 from './img/IMG_2365.PNG';
 import gallery8 from './img/CE_靜理央.png';
 import gallery9 from './img/寶貝畫給我ㄉ耶.png';
 import gallery10 from './img/MicheRoro_偵探怪盜日拾肆 (1).jpg';
-import Rio from './img/ebihara_rio.png';
-import Jyuushi from './img/mitarai_jyuushi.png';
-import Hinata from './img/miyazaki_hinata.png';
-import { Controller } from 'swiper/modules';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Slider() {
 
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(1);
+  
+
+
+  var img_list = [
+    {id: 0,name: gallery1,artist: "蘇伍柒"},
+    {id: 1,name: gallery2,artist: "哺會通靈"},
+    {id: 2,name: gallery3,artist: "巴拉魚"},
+    {id: 3,name: gallery4,artist: "거품"},
+    {id: 4,name: gallery5,artist: "거품"},
+    {id: 5,name: gallery6,artist: "茯黎"},
+    {id: 6,name: gallery7,artist: "茯黎"},
+    {id: 7,name: gallery8,artist: "CE"},
+    {id: 8,name: gallery9,artist: "ㄐ寶貝🥰"},
+    {id: 9,name: gallery10,artist: "MicheRoro"}
+  ];
+
+  const [countimg, setCountimg] = useState(img_list[0].name);
+
+  function handleShow(breakpoint,n) {
+    setFullscreen(breakpoint);
+    setShow(true);
+    setCount(n);
+    setCountimg(img_list[n].name);
+    //console.log(n);
+    console.log(img_list[n].name);
+  }
+
+  
+  
+  
     
     return (
         <div className='sli_al'>
             <Container className='sli'>
-        <h1>GALLERY</h1>
+        <h1 className='sl_h1'>GALLERY</h1>
         <Swiper slidesPerView={1} spaceBetween={10} 
         breakpoints={{
           640: {
@@ -57,49 +90,36 @@ function Slider() {
           disableOnInteraction: false,
         }}
       >
-            <SwiperSlide>
-                <img src={gallery1} />
-                <p className='sli_intro'>@蘇伍柒</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery2} />
-                <p className='sli_intro'>@哺會通靈</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery9} />
-                <p className='sli_intro'>@ㄐ寶貝🥰</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery3} />
-                <p className='sli_intro'>@巴拉魚</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery4} />
-                <p className='sli_intro'>@거품</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery5} />
-                <p className='sli_intro'>@거품</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery6} />
-                <p className='sli_intro'>@茯黎</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery7} />
-                <p className='sli_intro'>@茯黎</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery8} />
-                <p className='sli_intro'>@CE</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={gallery10} />
-                <p className='sli_intro'>@MicheRoro</p>
-            </SwiperSlide>
+            
+            
+            {img_list.map((gallery, index) => 
+              <SwiperSlide key={index} onClick={() => handleShow(true,index)}>
+                <img src={gallery.name} />
+                <p className='sli_intro'>@{gallery.artist}</p>
+              </SwiperSlide>
+              
+            )}
+
+            
+
       </Swiper>
       </Container>
-        </div>
+      
+      
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          Modal body content{count}
+          
+          <img src={countimg} />
+          
+          
+            
+          
+        </Modal.Body>
+      </Modal>
+
+      </div>
       
       
     );
